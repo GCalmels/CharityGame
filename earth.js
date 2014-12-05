c07d63a75d58aedebd86a924d45712bfbfbbd15b
bduvignaud <benoit.duvignaud@gmail.com>
2014-12-05 01:22:19 +0100

    loader and csv

--- earth.js
+++ earth.js
@@ -38,13 +38,45 @@ function init()
 	scene.add( light )
 
 	// EarthMesh
+	var manager = new THREE.LoadingManager();
+	var texture = new THREE.Texture();
+
+	var loader = new THREE.ImageLoader( manager );
+		loader.load( 'earthTexture.jpg', function ( image ) {
+			texture.image = image;
+			texture.needsUpdate = true;
+		} );
+
 	var geometry   = new THREE.SphereGeometry(0.5, 32, 32)
 	var material  = new THREE.MeshPhongMaterial()
-	material.map = THREE.ImageUtils.loadTexture('earthTexture.jpg')
+	material.map = texture
 	earthMesh = new THREE.Mesh(geometry, material)
 				
 	scene.add(earthMesh)
 
+	d3.csv("data.csv", type, function(error, data) {
+		data.forEach( function(value,index,array) {
+			var lat = value.lat
+			var lon =  value.lon
+
+			var pos = latLongToVector3(lat, lon, 0.5, 0)
+
+			var geometry = new THREE.SphereGeometry(0.02,32,32);
+			var material = new THREE.MeshBasicMaterial( { color: 0xFA2A2A } );
+			var cube = new THREE.Mesh( geometry, material );
+			cube.position.x = pos.x;
+			cube.position.y = pos.y;
+			cube.position.z = pos.z;
+
+			earthMesh.add(cube);
+		});
+	});
+/*
+	// 34.909568, 38.991521
+	// 45.926093, 4.832203
+
+
+*/
 	// Rendering
 	renderer = new THREE.WebGLRenderer({alpha:true});
 	renderer.setClearColor( 0xeeeeee, 1);
@@ -53,6 +85,12 @@ function init()
 
 	window.addEventListener( 'resize', onWindowResize, false );
 
+}
+
+function type(d)
+{
+	d.value = +d.value;
+	return d;
 }
 
 function onWindowResize() 
@@ -107,3 +145,13 @@ function render()
 	renderer.render( scene, camera );
 }
 		
+function latLongToVector3(lat, lon, radius, heigth) {
+        var phi = (lat)*Math.PI/180;
+        var theta = (lon-180)*Math.PI/180;
+ 
+        var x = -(radius+heigth) * Math.cos(phi) * Math.cos(theta);
+        var y = (radius+heigth) * Math.sin(phi);
+        var z = (radius+heigth) * Math.cos(phi) * Math.sin(theta);
+ 
+        return new THREE.Vector3(x,y,z);
+}
