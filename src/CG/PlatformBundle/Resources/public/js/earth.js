b5cd4ff1421e4905dc7f4d487e821cd2dec72cc7
GWillmann <gregwillm@gmail.com>
2014-12-05 02:39:22 +0100

    Intégration webgl ok

--- src/CG/PlatformBundle/Resources/public/js/earth.js
+++ src/CG/PlatformBundle/Resources/public/js/earth.js
@@ -41,26 +41,11 @@ function init()
 	var manager = new THREE.LoadingManager();
 	var texture = new THREE.Texture();
 
-	var onProgress = function ( xhr ) {
-		console.log('On Progress')
-	};
-
-	var onError = function ( xhr ) {
-		console.log('An error has occured')
-		console.log(xhr);
-	};
-
 	var loader = new THREE.ImageLoader( manager );
-	loader.load( '../img/earthTexture.jpg', function ( image ) {
-		texture.image = image;
-		texture.needsUpdate = true;
-		console.log('image loaded successfully')
-	}, onProgress, onError);
-
-
-
-
-
+		loader.load( '../img/earthTexture.jpg', function ( image ) {
+			texture.image = image;
+			texture.needsUpdate = true;
+		} );
 
 	var geometry   = new THREE.SphereGeometry(0.5, 32, 32)
 	var material  = new THREE.MeshPhongMaterial()
@@ -69,14 +54,43 @@ function init()
 
 	scene.add(earthMesh)
 
+	d3.csv("../data/data.csv", type, function(error, data) {
+		data.forEach( function(value,index,array) {
+			var lat = value.lat
+			var lon =  value.lon
+
+			var pos = latLongToVector3(lat, lon, 0.5, 0)
+
+			var geometry = new THREE.SphereGeometry(0.02,32,32);
+			var material = new THREE.MeshBasicMaterial( { color: 0xFA2A2A } );
+			var cube = new THREE.Mesh( geometry, material );
+			cube.position.x = pos.x;
+			cube.position.y = pos.y;
+			cube.position.z = pos.z;
+
+			earthMesh.add(cube);
+		});
+	});
+/*
+	// 34.909568, 38.991521
+	// 45.926093, 4.832203
+
+
+*/
 	// Rendering
 	renderer = new THREE.WebGLRenderer({alpha:true});
-	renderer.setClearColor( 0xeeeeee, 1);
-	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setClearColor( 0xffffff, 1);
+	renderer.setSize(window.innerWidth/2, window.innerHeight/2);
 	container.appendChild( renderer.domElement );
 
 	window.addEventListener( 'resize', onWindowResize, false );
 
+}
+
+function type(d)
+{
+	d.value = +d.value;
+	return d;
 }
 
 function onWindowResize()
@@ -87,46 +101,57 @@ function onWindowResize()
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 
-	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setSize( window.innerWidth / 2, window.innerHeight / 2 );
 }
 
 
 document.addEventListener('mousemove', function(event)
 {
 	if(mouse.isClicked)
-		{
-			mouse.x	= (event.clientX / window.innerWidth ) - 0.5
-			mouse.y	= (event.clientY / window.innerHeight) - 0.5
-		}
-	}, false)
-
-	document.addEventListener('mousedown', function(event)
 	{
-		mouse.isClicked = true;
-	})
-
-	document.addEventListener('mouseup', function(event)
-	{
-		mouse.isClicked = false;
-		mouse.x = 0;
-		mouse.y = 0;
-	})
-
-	function animate()
-	{
-		requestAnimationFrame( animate );
-		render();
+		mouse.x	= (event.clientX / window.innerWidth ) - 0.5
+		mouse.y	= (event.clientY / window.innerHeight) - 0.5
 	}
+}, false)
 
-	function render()
+document.addEventListener('mousedown', function(event)
+{
+	mouse.isClicked = true;
+})
+
+document.addEventListener('mouseup', function(event)
+{
+	mouse.isClicked = false;
+	mouse.x = 0;
+	mouse.y = 0;
+})
+
+function animate()
+{
+	requestAnimationFrame( animate );
+	render();
+}
+
+function render()
+{
+	if(!( (mouse.y>0 && earthMesh.rotation.x>1) || (mouse.y<0 && earthMesh.rotation.x<-1) ) )
 	{
-		if(!( (mouse.y>0 && earthMesh.rotation.x>1) || (mouse.y<0 && earthMesh.rotation.x<-1) ) )
-			{
-				earthMesh.rotation.x += mouse.y/10
-			}
-			earthMesh.rotation.y += mouse.x/5
+		earthMesh.rotation.x += mouse.y/10
+	}
+	earthMesh.rotation.y += mouse.x/5
 
-			camera.lookAt( scene.position )
+	camera.lookAt( scene.position )
 
-			renderer.render( scene, camera );
-		}
+	renderer.render( scene, camera );
+}
+
+function latLongToVector3(lat, lon, radius, heigth) {
+        var phi = (lat)*Math.PI/180;
+        var theta = (lon-180)*Math.PI/180;
+
+        var x = -(radius+heigth) * Math.cos(phi) * Math.cos(theta);
+        var y = (radius+heigth) * Math.sin(phi);
+        var z = (radius+heigth) * Math.cos(phi) * Math.sin(theta);
+
+        return new THREE.Vector3(x,y,z);
+}
