a9e3b8efd7c8953237472cf483e73681fd7e8b58
GCalmels <gabriel.calmels@hotmail.fr>
2014-12-05 01:12:41 +0100

    Add/view Event

---
+++ src/CG/PlatformBundle/Form/DonationKindType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace CG\PlatformBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DonationKindType extends AbstractType
+{
+     /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('description', 'text')
+            ->add('gain', 'text')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CG\PlatformBundle\Entity\DonationKind'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'cg_platformbundle_donation_kind';
+    }
+}
